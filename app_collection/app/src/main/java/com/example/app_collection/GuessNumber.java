a8972132cf420ead004590d8324761770c6de12b
kevin510622 <kevin510622@gmail.com>
2024-04-04 16:16:07 +0800

    apply theme color, and basic guess number game completed (has restart system)

--- app_collection/app/src/main/java/com/example/app_collection/GuessNumber.java
+++ app_collection/app/src/main/java/com/example/app_collection/GuessNumber.java
@@ -2,10 +2,14 @@ package com.example.app_collection;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.DialogInterface;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,16 +19,20 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-public class GuessNumber extends AppCompatActivity implements DialogInterface.OnClickListener {
-    String GameMode;
+public class GuessNumber extends AppCompatActivity{
+    String currentGameMode;
     String expectNumber = "";
     Spinner selectNumSpinner1;
     Spinner selectNumSpinner2;
     Spinner selectNumSpinner3;
     Spinner selectNumSpinner4;
     TextView warning_textView;
+    TextView player_guess;
+    TextView answer_debug;
+    TextView mode_text;
     int totalPlayTime = 0;
     String totalPlayLog = "";
+    Button buttonGuess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,21 +43,45 @@ public class GuessNumber extends AppCompatActivity implements DialogInterface.On
     }
 
     void Initial(){
-        GameMode = "NoRepeat";
-       selectNumSpinner1 = findViewById(R.id.numberSelection1);
-       selectNumSpinner2 = findViewById(R.id.numberSelection2);
-       selectNumSpinner3 = findViewById(R.id.numberSelection3);
-       selectNumSpinner4 = findViewById(R.id.numberSelection4);
+        currentGameMode = "NoRepeat";
+        selectNumSpinner1 = findViewById(R.id.numberSelection1);
+        selectNumSpinner2 = findViewById(R.id.numberSelection2);
+        selectNumSpinner3 = findViewById(R.id.numberSelection3);
+        selectNumSpinner4 = findViewById(R.id.numberSelection4);
+        mode_text = findViewById(R.id.mode_text);
+        buttonGuess = findViewById(R.id.GuessNumber);
+        answer_debug = findViewById(R.id.answer_debug);
+        player_guess = findViewById(R.id.player_guess);
+        ModeChange("NoRepeat");
        RandomNumber();
-
        warning_textView = findViewById(R.id.warning_text);
     }
 
-    void RandomNumber(){
-        totalPlayTime = 0;
+    void ModeChange(String gameMode){
+        switch (gameMode){
+            case "NoRepeat":
+                currentGameMode = gameMode;
+                mode_text.setText("沒有重複數字");
 
+                break;
+            case "HasRepeat":
+                mode_text.setText("可能出現重複數字");
+
+                break;
+        }
+    }
+    void RestartGame(){
+        totalPlayTime = 0;
+        totalPlayLog = "";
+        expectNumber = "";
+        answer_debug.setText("");
+        warning_textView.setText("");
+        player_guess.setText("");
+        RandomNumber();
+    }
+    void RandomNumber(){
         Random random = new Random();
-        switch (GameMode){
+        switch (currentGameMode){
             case "NoRepeat":
                 List<Integer> numbers = new ArrayList<>();
                 for (int i = 0; i < 10; i++) {
@@ -60,7 +92,6 @@ public class GuessNumber extends AppCompatActivity implements DialogInterface.On
                 for(int i=0;i< selectedNumbers.size();i++){
                     expectNumber += selectedNumbers.get(i);
                 }
-                System.out.println(selectedNumbers);
                 break;
             case "HasRepeat":
                 for (int i =0 ;i <4;i++){
@@ -69,8 +100,8 @@ public class GuessNumber extends AppCompatActivity implements DialogInterface.On
                 }
                 break;
         }
-        TextView textView = (TextView) findViewById(R.id.answer_debug);
-        textView.setText(expectNumber);
+        answer_debug.setText(expectNumber);
+
     }
 
     public void ClickButton_GuessNumber(View view){
@@ -82,7 +113,7 @@ public class GuessNumber extends AppCompatActivity implements DialogInterface.On
         String selectedValue4 = selectNumSpinner4.getSelectedItem().toString();
         String playerGuessNumber = selectedValue1 + selectedValue2 + selectedValue3 + selectedValue4;
 
-        switch (GameMode){
+        switch (currentGameMode) {
             case "NoRepeat":
                 outputText = NoRepeatGuess(playerGuessNumber);
                 break;
@@ -90,10 +121,9 @@ public class GuessNumber extends AppCompatActivity implements DialogInterface.On
                 break;
         }
 
-        TextView textView = (TextView) findViewById(R.id.player_guess);
         if(!outputText.equals("")){
             totalPlayLog = outputText + "\n" + totalPlayLog;
-            textView.setText(totalPlayLog);
+            player_guess.setText(totalPlayLog);
         }
     }
 
@@ -105,6 +135,12 @@ public class GuessNumber extends AppCompatActivity implements DialogInterface.On
                 return "";
             }
         }
+
+        if(totalPlayLog.contains(playerGuessNumber)){
+            warning_textView.setText("您已經猜過這個數字！");
+            return "";
+        }
+
         warning_textView.setText("");
         totalPlayTime++;
         int A = 0;
@@ -122,30 +158,52 @@ public class GuessNumber extends AppCompatActivity implements DialogInterface.On
             }
         }
 
+        if(A == 4){
+            WinGame();
+        }
+
         return "猜 " + totalPlayTime + "：            " + playerGuessNumber + "            結果: "+ A + "A" + B + "B";
     }
 
-    public void alertButtonClick(View view){
+    public void WinGame(){
+        warning_textView.setText("恭喜您！您猜對了！");
+        GenerateAlertTextBox("恭喜答對", "恭喜您！您猜對了！\n您總共猜了 " + totalPlayTime +" 次", "","確認", 2);
+        int color = ContextCompat.getColor(GuessNumber.this, R.color.gray_500); // 獲取顏色資源
+        buttonGuess.setBackgroundTintList(ColorStateList.valueOf(color)); // 改變按鈕顏色
+        buttonGuess.setEnabled(false);
+        buttonGuess.setTextColor(Color.BLACK); // 改變按鈕顏色
+    }
+    public void BackToTitleScreen(View view){
+        GenerateAlertTextBox("返回主畫面", "您確定要離開嗎？", "取消", "好的", 0);
+    }
+    public void RestartGame(View view){
+        GenerateAlertTextBox("重新開始遊戲", "您要重新生成一個新的數字嗎？", "取消", "好的", 1);
+    }
+
+    public void GenerateAlertTextBox(String Title, String Message, String NegativeMsg, String PositiveMsg, int type){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("返回主畫面")
-                .setMessage("您確定要離開嗎？")
-                .setNegativeButton("取消",this)
-                .setPositiveButton("好的",this)
+
+        builder.setTitle(Title)
+                .setMessage(Message)
+                .setNegativeButton(NegativeMsg, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // 取消的回應，這裡只是關閉對話框，不需要其他操作
+                    }
+                })
+                .setPositiveButton(PositiveMsg, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        if (type == 0) {
+                            // 離開的回應
+                            Toast.makeText(GuessNumber.this, "回到主畫面", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }else if(type == 1){
+                            RestartGame();
+                            Toast.makeText(GuessNumber.this, "重新生成數字完成！", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
                 .setCancelable(false)
                 .show();
     }
-
-    @Override
-    public void onClick(DialogInterface dialog, int which) {
-        switch (which){
-            case DialogInterface.BUTTON_POSITIVE:
-                Toast.makeText(this, "回到主畫面", Toast.LENGTH_SHORT).show();
-                finish();
-                break;
-            case DialogInterface.BUTTON_NEGATIVE:
-                break;
-        }
-    }
-
-
 }
